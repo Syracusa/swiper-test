fcee09c0a71f8ccbbdb300ee775c7c4955aa52a0
Syracusa <msrndsy@gmail.com>
2023-07-20 14:21:23 +0900

    Update test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swiper-test';
+  
+  @ViewChild('swiperParentRef')
+  swiperParentRef: ElementRef | undefined;
+  swiperParent?: Swiper;
+  
+  @ViewChild('swiperChildRef')
+  swiperChildRef: ElementRef | undefined;
+  swiperChild?: Swiper;
+  
+  ngAfterViewInit() {
+    register();
+    this.swiperParent = this.swiperParentRef?.nativeElement.swiper;
+    this.swiperParent?.mousewheel.enable();
+    
+    /* Only enable child mousewheel if parent's activeIndex is 1 */
+    // this.swiperParent?.on('slideChangeTransitionStart', () => {
+    //   console.log('ParentSlideChangeTransitionStart', this.swiperParent?.activeIndex);
+    //   if (this.swiperParent?.activeIndex === 1) {
+    //     this.swiperChild?.mousewheel.enable();
+    //   } else {
+    //     this.swiperChild?.mousewheel.disable();
+    //   }
+    // });
+
+    this.swiperParent?.on('slideChangeTransitionEnd', () => {
+      console.log('ParentSlideChangeTransitionEnd', this.swiperParent?.activeIndex);
+    });
+
+    /* Mouse scroll event handle */
+    this.swiperParent?.on('scroll', (s, e) => {
+      console.log('scroll', s, e);
+    });
+    
+    this.swiperChild = this.swiperChildRef?.nativeElement.swiper;
+    this.swiperChild?.mousewheel.enable();
+    this.swiperChild?.on('slideChangeTransitionEnd', () => {
+      console.log('childSlideChangeTransitionEnd', this.swiperChild?.activeIndex);
+    });
+
+
+  }
 }
